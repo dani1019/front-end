04f231665c96bf32715317ea335c76b21297b231
dani1019 <nihonkita17@gmail.com>
2022-02-13 16:10:28 +0900

    game-setting, image-range
    game-setting, image-range

---
+++ carrot-game/main.js
@@ -0,0 +1,13 @@
+const bug = document.querySelector('.bug');
+const carrot = document.querySelector('.carrot');
+
+document.addEventListener('click',(event)=>{
+    const top = Math.floor(Math.random()*900);
+
+    const left = Math.floor(Math.random()*1300);
+    //500=<top=<800の場合、bug,carrot表示される
+    // if(top)
+    //100=<left=<1200の場合、bug,carrot表示される
+    // if(left>= 500 && left <=800){
+    }
+});
