95260d9ade6dd78a6518c0d2bce370793001bb2f
dani1019 <nihonkita17@gmail.com>
2022-02-16 19:23:05 +0900

    carrot image make
    carrot image make

--- carrot-game/main.js
+++ carrot-game/main.js
@@ -1,25 +1,31 @@
 const bug = document.querySelector('.bug');
 const carrot = document.querySelector('.carrot');
+const button__play = document.querySelector('.button__play');
+const canvas = document.querySelector('.canvas')
+const ctx = canvas.getContext('2d');
 
-document.addEventListener('click',(event)=>{
-    const imgUrl = 'img/carrot.png';
+function game__start(){
+    button__play.innerHTML=`
+    <i class="fas fa-stop"></i>
+    `
+    image__make();
+}
+function image__make(){
     let imgs = [];
-    let imgPos = [];
-
-    for (let i = 0; i < 50; i++){
-        // imgs.push(new Image());
-        // imgs[i].scr = imgUrl;
-        // imgs[i].style.position = 'absolute';
-
-        imgPos[i] = [Math.floor(Math.random() * window.innerWidth),
-                    Math.floor(Math.random() * window.innerHeight)];
-        console.log(imgPos[i]);  
-
-        bug.style.left = imgPos[i][0] + 'px';
-        bug.style.top = imgPos[i][1] + 'px';
-        // document.body.appendChild(imgs[i]);
-        // console.log(imgs[i].style.left,imgs[i].style.top);
+    let width = 0;
+    let height = 0;
+    for (let i = 0; i < 11; i++){
+        width = Math.floor(Math.random() * window.innerWidth);
+        height = Math.floor(Math.random() * window.innerHeight);
+        imgs.push([width,height]);
     }
-
-
+    for(let i = 0; i< imgs.length; i++){
+        const x = imgs[i][0];
+        const y = imgs[i][1];
+        ctx.drawImage(carrot,x,y);
+    }
+}
+button__play.addEventListener('click',()=>{
+    game__start();
 });
+
